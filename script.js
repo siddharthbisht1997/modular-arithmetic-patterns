eb449fdb36a1da0c4cdc611033c3d21bf23d14a2
Siddharth Bisht <siddharthbisht97@gmail.com>
2023-11-26 12:53:02 +0530

    Fixed Xprime position error
    The Modulus operation was giving a correct output but incorrect points were getting connected. This was due to the code having Pi/Div and not 2*Pi/Div. This has been corrected and now the Patterns are as expected.

--- script.js
+++ script.js
@@ -13,117 +13,49 @@ const ctx = canvas.getContext('2d');
 // Setting Context configurations for drawing
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
-ctx.lineWidth = 0.05;
+ctx.lineWidth = 0.005;
 
 // The below code creates a circle on the canvas to visualize the
 // number of points or divisions (p)
-let radius = 200;
-let p = 10;
-let maxIterations = 10;
+let radius = 100;
 
 
-/**
- * Draws a set of points on the outline of a circle
- * with the circle positioned in the center of the canvas
- * @param  {Object} ctx Canvas Context
- * @param  {Object} cv Canvas Object
- * @param  {Number} radius Radius of the circle
- * @param  {Number} p Number of divisions on the circle
- */
-function drawPoints(ctx, cv, radius, p) {
 
-    // Clear the canvas
-    ctx.clearRect(0, 0, cv.width, cv.height);
-    // console.log(cv.width, cv.height)
-    // console.log("Drawing points");
 
-    // Draw the points as per the number of divisions needed
-    for (let theta = 0; theta < 2 * Math.PI; theta += Math.PI / p) {
 
-        const x = radius * Math.cos(theta) + cv.width / 2;
-        const y = radius * Math.sin(theta) + cv.height / 2;
+let p = 1;
 
-        // console.log(x,y)
-        ctx.beginPath();
-        ctx.arc(x, y, 1, 0, 2 * Math.PI);
-        ctx.closePath();
-        ctx.fill();
-    }
-}
+function connectPoints() {
 
-/**
- * Simple mathematical function
- * @param  {Number} x Input to the function
- * @returns {Number} Output of the function
- */
-function getXPrime(x) {
-    return 2*x;
-}
-
-/**
- * Runs the modular arithmetic and connects the point
- * as per the following construct connect x and x' where
- * x' = f(x) mod p where p is the number of divisions 
- * @param  {Object} ctx Canvas Context
- * @param  {Object} cv Canvas Object
- * @param  {Number} radius Radius of the circle
- * @param  {Number} p Number of divisions on the circle
- */
-function connectPoints(ctx, cv, radius, p) {
-
-    ctx.beginPath();
-
-    for(let i = 0; i <= maxIterations; i++){
-        let iPrime = getXPrime(i)%(p);
-        // console.log(i,iPrime);
-        
-        // console.log(radius * Math.cos(i  * Math.PI / p) + cv.width/2, radius * Math.sin(i * Math.PI / p) + cv.height/2);
-        // console.log(radius * Math.cos(iPrime  * Math.PI / p) + cv.width/2, radius * Math.sin(iPrime * Math.PI / p)  + cv.height/2);
-
-        ctx.moveTo(
-            radius * Math.cos(i  * Math.PI / p) + cv.width/2,
-            radius * Math.sin(i * Math.PI / p) + cv.height/2
-        );
-        ctx.lineTo(
-            radius * Math.cos(iPrime * Math.PI / p) + cv.width/2,
-            radius * Math.sin(iPrime * Math.PI / p) + cv.height/2
-        );
-        ctx.stroke();
-
-    }
-}
-
-/**
- * Runs the modular arithmetic and connects the point
- * as per the following construct connect x and x' where
- * x' = f(x) mod p where p is the number of divisions 
- * @param  {Number} i Number of divisions on the circle
- */
-function connectSinglePoint() {
-
-    if (iteration>100){
+    if (p>200){
         console.log('Animation halted.');
         return 0;
     }
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    let iPrime = getXPrime(iteration)%(p);
+    let index = 0;
+    // console.log("-----------------"+p);
+    for(let m = 2; m < 6; m++){
+        for(let i = 1; i < p; i++){
+            // ctx.lineWidth = 0.01/(p-i+1);
+            let iPrime = (m*i)%(p);
 
-    ctx.moveTo(
-        radius * Math.cos(iteration  * Math.PI / p) + canvas.width/2,
-        radius * Math.sin(iteration * Math.PI / p) + canvas.height/2
-    );
-    ctx.lineTo(
-        radius * Math.cos(iPrime * Math.PI / p) + canvas.width/2,
-        radius * Math.sin(iPrime * Math.PI / p) + canvas.height/2
-    );
-    ctx.stroke();
-    
-    requestAnimationFrame(connectSinglePoint);
-    iteration += 1;
+            ctx.moveTo(
+                radius * Math.cos(i * (2 * Math.PI)/ p) + canvas.width*((index+1)/5),
+                radius * Math.sin(i * (2 * Math.PI)/ p) + canvas.height/2
+            );
+            ctx.lineTo(
+                radius * Math.cos(iPrime * (2 * Math.PI)/ p) + canvas.width*((index+1)/5),
+                radius * Math.sin(iPrime * (2 * Math.PI)/ p) + canvas.height/2
+            );
+            ctx.stroke();
+        }
+    index += 1;   
+    }
+
+    p += 1;
+    requestAnimationFrame(connectPoints);
 }
 
-drawPoints(ctx, canvas, radius, p);
-// connectPoints(ctx, canvas, radius, p);
-let iteration = 1;
-connectSinglePoint();
+connectPoints();
